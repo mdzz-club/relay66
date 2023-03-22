f57b1e1b2b5f3eb1d063a08a4cee14e458511b97
xbol0 <xbolo@duck.com>
2023-03-22 10:21:00 +0800

    fix:req params contains null

--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 import { nostring, serve } from "./deps.ts";
 import { onEvent, onReq, onStream } from "./handler.ts";
 import { spam } from "./spam.ts";
+import { PgRepository } from "./pg_repo.ts";
 
 const dbUrl = Deno.env.get("DB_URL") || "postgres://127.0.0.1:5432/nostring";
-const db = new nostring.PgRepository(dbUrl);
+const db = new PgRepository(dbUrl);
 await db.init();
 
 await spam.start();
--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 export * as nostring from "https://deno.land/x/nostring@2.1.1/mod.ts";
 export { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+export * from "https://deno.land/x/zod@v3.21.4/mod.ts";
+export * as pg from "https://deno.land/x/postgres@v0.17.0/mod.ts";
